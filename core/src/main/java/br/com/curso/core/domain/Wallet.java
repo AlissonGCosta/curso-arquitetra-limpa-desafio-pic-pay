657ca4db2e25f357c5759ea9fd7391a5c36db91e
Alisson G Costa <alisson.costa84200@gmail.com>
2026-06-01 23:10:26 -0300

    feat(core): add wallet

---
+++ core/src/main/java/br/com/curso/core/domain/Wallet.java
@@ -0,0 +1,81 @@
+package br.com.curso.core.domain;
+
+import br.com.curso.core.exception.TrasnferException;
+import br.com.curso.core.exception.enums.ErrorCodeEnum;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Wallet {
+
+    private Long id;
+    private BigDecimal balance;
+    private User user;
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
+
+    public Wallet(BigDecimal balance, Long id, LocalDate createdAt, LocalDate updatedAt, User user) {
+        this.balance = balance;
+        this.id = id;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.user = user;
+    }
+
+    public Wallet(BigDecimal balance, User user) {
+        this.balance = balance;
+        this.user = user;
+        this.createdAt = LocalDate.now();
+    }
+
+    public Wallet() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDate updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+   public void receivValue(BigDecimal value) {
+        this.balance.add(value);
+   }
+
+    public void transfer(BigDecimal value) {
+        if(this.user.getType() == UserTypeEnum.SHOPKEEPER){
+            throw new TrasnferException(ErrorCodeEnum.ON0001.getMessage(), ErrorCodeEnum.ON0001.getCode());
+        }
+
+        if(this.balance.compareTo(value) < 0){
+            throw
+        }
+
+        this.balance.subtract(value);
+    }
+}
