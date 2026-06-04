d0fa97859e859f926f9cf9659879ff7dba6168ee
Alisson G Costa <alisson.costa84200@gmail.com>
2026-06-03 22:03:56 -0300

    feat(usecase): define user notification contract

--- usecase/src/main/java/br/com/curso/usecase/UserNotificationUseCase.java
+++ usecase/src/main/java/br/com/curso/usecase/UserNotificationUseCase.java
@@ -1,4 +1,7 @@
 package br.com.curso.usecase;
 
+import br.com.curso.core.domain.Transaction;
+
 public interface UserNotificationUseCase {
+    Boolean notificate(Transaction transaction, String email);
 }
