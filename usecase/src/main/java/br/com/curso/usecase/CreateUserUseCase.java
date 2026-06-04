1386e6477fd7c94b34db28eb5ad7a6029c9055f6
Alisson G Costa <alisson.costa84200@gmail.com>
2026-06-03 23:48:59 -0300

    feat(usecase): expand create user contract

--- usecase/src/main/java/br/com/curso/usecase/CreateUserUseCase.java
+++ usecase/src/main/java/br/com/curso/usecase/CreateUserUseCase.java
@@ -1,7 +1,9 @@
 package br.com.curso.usecase;
 
 import br.com.curso.core.domain.User;
+import br.com.curso.core.exception.EmailException;
+import br.com.curso.core.exception.TaxNumberException;
 
 public interface CreateUserUseCase {
-    void create(User user);
+    void create(User user, String pin) throws TaxNumberException, EmailException;
 }
