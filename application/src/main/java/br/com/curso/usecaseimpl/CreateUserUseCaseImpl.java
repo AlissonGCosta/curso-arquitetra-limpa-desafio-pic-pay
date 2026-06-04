3938bc20415b820ddc8bf5bcfba9812a40e22f58
Alisson G Costa <alisson.costa84200@gmail.com>
2026-06-03 23:48:58 -0300

    feat(application): implement create user use case

---
+++ application/src/main/java/br/com/curso/usecaseimpl/CreateUserUseCaseImpl.java
@@ -0,0 +1,52 @@
+package br.com.curso.usecaseimpl;
+
+import br.com.curso.core.domain.TransactionPin;
+import br.com.curso.core.domain.User;
+import br.com.curso.core.domain.Wallet;
+import br.com.curso.core.exception.EmailException;
+import br.com.curso.core.exception.TaxNumberException;
+import br.com.curso.core.exception.enums.ErrorCodeEnum;
+import br.com.curso.gateway.CreateUserGateway;
+import br.com.curso.usecase.*;
+
+import java.math.BigDecimal;
+
+public class CreateUserUseCaseImpl implements CreateUserUseCase {
+
+    private CreateUserGateway createUserGateway;
+    private TaxNumberAvaliableUseCase taxNumberAvaliableUseCase;
+    private EmailAvailableUseCase emailAvailableUseCase;
+    private CreateWalletUseCase createWalletUseCase;
+    private CreateTransactionPinUseCase createTransactionPinUseCase;
+
+    public CreateUserUseCaseImpl(TaxNumberAvaliableUseCase taxNumberAvaliableUseCase ,
+                                 EmailAvailableUseCase emailAvailableUseCase,
+                                 CreateUserGateway createUserGateway,
+                                 CreateTransactionPinUseCase createTransactionPinUseCase,
+                                 CreateWalletUseCase createWalletUseCase) {
+        this.taxNumberAvaliableUseCase = taxNumberAvaliableUseCase;
+        this.emailAvailableUseCase = emailAvailableUseCase;
+        this.createUserGateway = createUserGateway;
+        this.createTransactionPinUseCase = createTransactionPinUseCase;
+        this.createWalletUseCase = createWalletUseCase;
+    }
+
+    @Override
+    public void create(User user, String pin) throws TaxNumberException, EmailException {
+
+        if(!taxNumberAvaliableUseCase.taxNumberAvailable(user.getTaxNumber().getValue())){
+            throw new TaxNumberException(ErrorCodeEnum.ON0002.getMessage(), ErrorCodeEnum.ON0002.getCode());
+        }
+
+        if(!emailAvailableUseCase.emailAvailable(user.getEmail())){
+            throw new EmailException(ErrorCodeEnum.ON0003.getMessage(), ErrorCodeEnum.ON0003.getCode());
+        }
+
+        var userSaved = createUserGateway.create(user);
+
+        createWalletUseCase.create(new Wallet(BigDecimal.ZERO, userSaved));
+        createTransactionPinUseCase.create(new TransactionPin(userSaved, pin ));
+    }
+
+
+}
