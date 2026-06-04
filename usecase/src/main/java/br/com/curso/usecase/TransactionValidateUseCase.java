3279f6f6b402d8eb106f56ed7d470ff95956e766
Alisson G Costa <alisson.costa84200@gmail.com>
2026-06-03 22:03:56 -0300

    feat(usecase): define transaction validation contract

--- usecase/src/main/java/br/com/curso/usecase/TransactionValidateUseCase.java
+++ usecase/src/main/java/br/com/curso/usecase/TransactionValidateUseCase.java
@@ -1,4 +1,7 @@
 package br.com.curso.usecase;
 
+import br.com.curso.core.domain.Transaction;
+
 public interface TransactionValidateUseCase {
+    Boolean validateTransaction(Transaction transaction);
 }
