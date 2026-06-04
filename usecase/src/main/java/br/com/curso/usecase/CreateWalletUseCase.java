ebad2348d91e7dc36a4a9a12d0f9ead8434b3a32
Alisson G Costa <alisson.costa84200@gmail.com>
2026-06-03 23:48:59 -0300

    refactor(usecase): correct wallet use case contract

---
+++ usecase/src/main/java/br/com/curso/usecase/CreateWalletUseCase.java
@@ -0,0 +1,9 @@
+package br.com.curso.usecase;
+
+import br.com.curso.core.domain.User;
+import br.com.curso.core.domain.Wallet;
+
+
+public interface CreateWalletUseCase {
+    void create(Wallet wallet);
+}
